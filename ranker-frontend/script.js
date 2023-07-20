1c2b9baeb90a77fb44d3c97bf0799f7783efeaf6
mattia <mattiarizzi80@gmail.com>
2023-07-20 22:33:26 +0200

    gif cringe

--- ranker-frontend/script.js
+++ ranker-frontend/script.js
@@ -1,13 +1,25 @@
 function inviaNick() {
     let nick=document.getElementById("nick").value;
-
     let theUrl = "http://localhost:8080/load-series?username="+nick
-    var xmlHttp = new XMLHttpRequest();
-    xmlHttp.open( "GET", theUrl, false ); // false for synchronous request
-    xmlHttp.send( null );
+
+    const xhr = new XMLHttpRequest();
+    xhr.open("GET", theUrl, true);
+    xhr.onload = (e) => {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            window.location.href = 'serie.html';
+        } else {
+            console.error(xhr.statusText);
+        }
+    };
+    document.getElementById("form-container").style.display="none";
+    document.getElementById("imm-container").style.display="block";
+
+
+    xhr.onerror = (e) => {
+        console.error(xhr.statusText);
+    };
+    xhr.send(null);
     // todo
-    // usare un altro metodo per fare la richiesta get al server backend
-    // subito dopo aver inviato la richiesta fai apparire un qualcosa che mostra che sta caricando
-    // una volta che il server ha risposto alla richiesta, cambia pagina
+    // una volta che il server ha risposto alla richiesta, cambia pagina (serie.html)
     // ...document..location = series...
 }
