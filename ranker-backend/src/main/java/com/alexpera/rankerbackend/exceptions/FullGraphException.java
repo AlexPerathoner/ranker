3ed2521b44652a97448b57bb2b2274dee99bbdbb
Alex Perathoner <alex.perathoner@gmail.com>
2023-07-29 11:03:13 +0200

    add FullGraphException, add todoso

---
+++ ranker-backend/src/main/java/com/alexpera/rankerbackend/exceptions/FullGraphException.java
@@ -0,0 +1,8 @@
+package com.alexpera.rankerbackend.exceptions;
+
+public class FullGraphException extends RuntimeException {
+
+    public FullGraphException(String s) {
+        super(s);
+    }
+}
--- ranker-backend/src/main/java/com/alexpera/rankerbackend/service/RankerService.java
+++ ranker-backend/src/main/java/com/alexpera/rankerbackend/service/RankerService.java
@@ -8,6 +8,7 @@ import com.alexpera.rankerbackend.dao.repo.MediaRepository;
 import com.alexpera.rankerbackend.dao.repo.UserRepository;
 import com.alexpera.rankerbackend.dao.repo.UsersMediaRepository;
 import com.alexpera.rankerbackend.exceptions.EmptyGraphException;
+import com.alexpera.rankerbackend.exceptions.FullGraphException;
 import com.alexpera.rankerbackend.exceptions.LoopException;
 import com.alexpera.rankerbackend.model.anilist.EdgeGraph;
 import com.alexpera.rankerbackend.model.anilist.RankedMedia;
@@ -44,25 +45,44 @@ public class RankerService {
     }
 
 
-    public Set<Media> getNextComparison(String username) throws EmptyGraphException {
+    public Set<Media> getNextComparison(String username) throws FullGraphException, EmptyGraphException {
+        // todo change, shouldnt return two media if there is already an edge between them
+
+        // todo rewrite consdering that it should behave like a binary search:
+        //      a b c d e f g
+        // first compare with d, then f, then e / g -> also depends on existing edges (consider them)
+        // first part to find first item of edge is good: consider item with least total count of edges
         // find item with lowest number of incoming or outgoing edges
         RankedMedia minItem = null;
         int minEdges = Integer.MAX_VALUE;
-        for (RankedMedia item : getGraph(username).vertexSet()) {
-            int edges = getGraph(username).inDegreeOf(item) + getGraph(username).outDegreeOf(item);
+        Graph<RankedMedia, DefaultEdge> graph = getGraph(username);
+        for (RankedMedia item : graph.vertexSet()) {
+            int edges = graph.inDegreeOf(item) + getGraph(username).outDegreeOf(item);
             if (edges < minEdges) {
                 minEdges = edges;
                 minItem = item;
             }
         }
+        // check outgoing edges of minItem. If # = vertex # * 2, graph is complete (loops are allowed)
 
+        // todo refactor
         // get mid item
-        List<RankedMedia> items = getItemsSorted(username);
-        RankedMedia midItem = items.get(items.size() / 2);
+        RankedMedia finalMinItem = minItem;
+        List<RankedMedia> items = getItemsSorted(username).stream()
+                .filter(t -> graph.containsEdge(finalMinItem,t))
+                .toList();
+
+        if (items.isEmpty()) {
+            throw new FullGraphException("All possible comparison for user " + username + " have already been made");
+        }
+
+        int index = items.size() / 2;
+        RankedMedia midItem = items.get(index);
 
         if (minItem == null) {
             throw new EmptyGraphException("No items in graph for user " + username + " to compare.");
         }
+        int vertexCount = graph.vertexSet().size();
         return Set.of(minItem, midItem);
     }
 
